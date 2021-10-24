25406b2bf73b0c6d774a16b90b8f2e582c08c18a
krigora <ihor.kryvynskyy@icloud.com>
2021-10-24 13:15:42 -0500

    creating TC5_SmartBearLoginTest

---
+++ src/test/java/com/cybertek/tests/day03_locators_intro/TC5_SmartBearLoginTest.java
@@ -0,0 +1,27 @@
+package com.cybertek.tests.day03_locators_intro;
+
+public class TC5_SmartBearLoginTest {
+
+    /*
+    TC #5:	Basic	login	authentication
+1- Open	a	chrome	browser
+2- Go	to:
+http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+3- Verify	title	equals:
+Expected:	Web	Orders	Login
+4- Enter	username:	Tester
+5- Enter	password:	test
+6- Click	“Sign	In”	button
+7- Verify	title equals:
+Expected:	Web	Orders
+     */
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+}
