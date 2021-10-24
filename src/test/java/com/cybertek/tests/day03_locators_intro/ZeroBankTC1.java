456602c5ad86b2360884a30b059625e4c08479b5
krigora <ihor.kryvynskyy@icloud.com>
2021-10-24 14:56:38 -0500

    creating ZeroBankTC1

---
+++ src/test/java/com/cybertek/tests/day03_locators_intro/ZeroBankTC1.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests.day03_locators_intro;
+
+public class ZeroBankTC1 {
+    /*
+    1. Open Chrome browser
+2. Go to http://zero.webappsecurity.com/login.html
+3. Verify header text
+Expected: “Log in to ZeroBank”
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+}
