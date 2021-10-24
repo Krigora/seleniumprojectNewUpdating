0f9c682778cf64cb4c69b01b4c3ea0ab4e568328
krigora <ihor.kryvynskyy@icloud.com>
2021-10-24 14:42:46 -0500

    updating TC5_SmartBearLoginTest

--- src/test/java/com/cybertek/tests/day03_locators_intro/TC5_SmartBearLoginTest.java
+++ src/test/java/com/cybertek/tests/day03_locators_intro/TC5_SmartBearLoginTest.java
@@ -1,5 +1,10 @@
 package com.cybertek.tests.day03_locators_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TC5_SmartBearLoginTest {
 
     /*
@@ -16,7 +21,44 @@ Expected:	Web	Orders	Login
 Expected:	Web	Orders
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        String appUrl = "http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx";
+        String userName = "Tester";
+        String password = "test";
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get(appUrl);
+
+        // enter username
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys(userName);
+        // enter password
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys(password);
+        // click login
+        driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+
+        //verify title is "web Orders
+
+        Thread.sleep(2000);
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Web Orders";
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("PASS: Login success/ title match");
+        } else {
+            System.out.println("FAIL: Login faild. title mismatched");
+
+        }
+
+        //click logout link
+        driver.findElement(By.linkText("logout")).click();
+        Thread.sleep(2000);
+
+        //quit browser
+      driver.quit();
+
 
 
 
