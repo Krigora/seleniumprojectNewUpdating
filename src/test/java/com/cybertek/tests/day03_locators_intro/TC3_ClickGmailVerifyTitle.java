fdf33ddb88c703da6d13ba725742a892cac8f215
krigora <ihor.kryvynskyy@icloud.com>
2021-10-24 10:54:19 -0500

    Creating TC4_GoogleSearch and updating TC3_ClickGmailVerifyTitle

--- src/test/java/com/cybertek/tests/day03_locators_intro/TC3_ClickGmailVerifyTitle.java
+++ src/test/java/com/cybertek/tests/day03_locators_intro/TC3_ClickGmailVerifyTitle.java
@@ -1,5 +1,10 @@
 package com.cybertek.tests.day03_locators_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TC3_ClickGmailVerifyTitle{
     /*
     TC #3: Back and forth navigation
@@ -13,11 +18,55 @@ Expected: Gmail
 Expected: Google
      */
 
+    public static void main(String[] args) {
+
+ WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+
+        String url = "https://google.com";   // те саме  driver.get("http://practice.cybertekschool.com");
+
+        driver.get(url);                     // те свме
+
+        // find Gmail link and click on it
+      //  driver.findElement(By.linkText("Gmail")).click();
+        driver.findElement(By.partialLinkText("Gm")).click();
+
+
+// Verify title contains/starts with Gmail
+        String expectedTitle = "Gmail";          //"Gmail: Free, Private & Secure Email | Google Workspace";
+
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.contains(expectedTitle)){
+            System.out.println("PASS: gmail title verification successful");
+        } else {
+            System.out.println("FAIL: gmail title verification successful");
+        }
+
+       // go back
+
+        driver.navigate().back();
+
+        String googleExpectedTitle = "Google";
+        String googleActualTitle = driver.getTitle();
+
+        if (googleActualTitle.equals(googleExpectedTitle)){
+            System.out.println("PASS: google title verification successful");
+        } else {
+            System.out.println("FAIL: google title verification successful");
+        }
+
+        driver.quit();
 
 
 
 
 
+
+}
 
 
 
---
+++ src/test/java/com/cybertek/tests/day03_locators_intro/TC4_GoogleSearch.java
@@ -0,0 +1,9 @@
+package com.cybertek.tests.day03_locators_intro;
+
+public class TC4_GoogleSearch {
+    public static void main(String[] args) {
+        
+    }
+
+
+}
