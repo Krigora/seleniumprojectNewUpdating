ee292567233a75caf973097e33d8eb6f3b9d3522
krigora <ihor.kryvynskyy@icloud.com>
2021-10-24 09:17:41 -0500

    Creating TC3_ClickGmailVerifyTitle

---
+++ src/test/java/com/cybertek/tests/day03_locators_intro/TC3_ClickGmailVerifyTitle.java
@@ -0,0 +1,40 @@
+package com.cybertek.tests.day03_locators_intro;
+
+public class TC3_ClickGmailVerifyTitle{
+    /*
+    TC #3: Back and forth navigation
+1- Open a chrome browser
+2- Go to: https://google.com
+3- Click to Gmail from top right.
+4- Verify title contains:
+Expected: Gmail
+5- Go back to Google by using the .back();
+6- Verify title equals:
+Expected: Google
+     */
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
