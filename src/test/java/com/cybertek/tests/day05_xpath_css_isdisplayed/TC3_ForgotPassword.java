808511456aa92c95fe26c5d8cfa4e941ada0acae
krigora <ihor.kryvynskyy@icloud.com>
2021-10-31 10:51:51 -0500

    creating new packet util and new class WebDriverFactory

--- src/test/java/com/cybertek/tests/day05_xpath_css_isdisplayed/TC3_ForgotPassword.java
+++ src/test/java/com/cybertek/tests/day05_xpath_css_isdisplayed/TC3_ForgotPassword.java
@@ -8,18 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC3_ForgotPassword {
 
-   /*
-   TC #3: PracticeCybertek/ForgotPassword URL verification
-1. Open Chrome browser
-2. Go to http://practice.cybertekschool.com/forgot_password
-3. Enter any email into input box
-4. Click on Retrieve password
-5. Verify URL contains:
-Expected: “email_sent”
-6. Verify textbox displayed the content as expected.
-Expected: “Your e-mail’s been sent!”
-Hint: You need to use getText method for this practice
-    */
+    /*
+    TC #3: PracticeCybertek/ForgotPassword URL verification
+ 1. Open Chrome browser
+ 2. Go to http://practice.cybertekschool.com/forgot_password
+ 3. Enter any email into input box
+ 4. Click on Retrieve password
+ 5. Verify URL contains:
+ Expected: “email_sent”
+ 6. Verify textbox displayed the content as expected.
+ Expected: “Your e-mail’s been sent!”
+ Hint: You need to use getText method for this practice
+     */
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -41,26 +41,25 @@ Hint: You need to use getText method for this practice
         } else {
             System.out.println("FAIL:email_sent url Failed");
         }
-            // verify Your e-mails been sent! is display
-            WebElement message = driver.findElement(By.name("confirmation_message"));
-            System.out.println("message displayed = " + message.getText());
+        // verify Your e-mails been sent! is display
+        WebElement message = driver.findElement(By.name("confirmation_message"));
+        System.out.println("message displayed = " + message.getText());
 
-//<h4 name="confirmation_message">Your e-mail's been sent!</h4>
-            String expMessage = "Your e-mails been sent!";
-            String actMessage = message.getText();
+        //<h4 name="confirmation_message">Your e-mail's been sent!</h4>
 
-            if (actMessage.equals(expMessage)) {
-                System.out.println("PASS: message displayed correctly");
+        String expMessage = "Your e-mail's been sent!";
+        String actMessage = message.getText();
 
-            } else {
+        if (actMessage.equals(expMessage)) {
+            System.out.println("PASS: message displayed correctly");
 
-                System.out.println("FAIL: message is not displayed expected");
-            }
-            System.out.println("Name attribute value of message element = " + message.getAttribute("name"));
+        } else {
 
-            driver.quit();
+            System.out.println("FAIL: message is not displayed expected");
+        }
+        System.out.println("Name attribute value of message element = " + message.getAttribute("name"));
 
-
+        driver.quit();
 
 
     }
---
+++ src/test/java/com/cybertek/utils/WebDriverFactory.java
@@ -0,0 +1,42 @@
+package com.cybertek.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    /*
+    WebDDriverFactory class:
+    is used to create WebDriver(selenium) object and return it
+    It will open the browser and maximize it
+
+    method:
+
+    getDrive(String browserType)
+    it will check browser type and return object;
+    if browserType is "chrome:"
+    it will set up chrome driver, and return new ChromeDriver
+    if browserType is Firefox":
+    it will set up firefox driver, and return new FirefoxDriver
+
+    EX:
+
+    WebDriver driver = WebDriverFactory.getDriver("Chrome");
+    and use driver as normal
+     */
+
+    public static WebDriver getDriver(String browserType){
+        if (browserType.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if (browserType.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+
+        } else {
+            System.out.println("Invalid BrowserType = " + browserType);
+            return null;
+        }
+    }
+}
