1e425c7b0931566a80066e59283cbdf1c99ee87a
krigora <ihor.kryvynskyy@icloud.com>
2021-10-31 14:39:51 -0500

    updating new class WorldPopulationXpathTest

--- src/test/java/com/cybertek/tests/day05_xpath_css_isdisplayed/GoogleMapsXpathTest.java
+++ src/test/java/com/cybertek/tests/day05_xpath_css_isdisplayed/GoogleMapsXpathTest.java
@@ -15,7 +15,7 @@ public class GoogleMapsXpathTest {
 
         //search for cybertek school
         WebElement addressField = driver.findElement(By.xpath("/html/body/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/form/div/div[3]/div/input[1]"));
-        addressField.sendKeys("cybertek school" + Keys.ENTER);
+        addressField.sendKeys("cybertek chicago" + Keys.ENTER);
 
 
         Thread.sleep(2000);
@@ -24,8 +24,9 @@ public class GoogleMapsXpathTest {
         WebElement directions = driver.findElement(By.xpath("/html/body/div[3]/div[9]/div[8]/div/div[1]/div/div/div[4]/div[1]/button/span/img"));
         directions.click();
 
-        WebElement startingAddressField =   driver.findElement(By.xpath("/input[@placeholder = 'Choose starting point, or click on the map...']"));
-        startingAddressField.sendKeys("Charlot, NC" + Keys.ENTER);
+
+        WebElement startingAddressField = driver.findElement(By.xpath("//input[@placeholder='Choose starting point, or click on the map...']"));
+        startingAddressField.sendKeys("8104 amethyst dr, Mclean VA" + Keys.ENTER);
 
         driver.quit();
 
