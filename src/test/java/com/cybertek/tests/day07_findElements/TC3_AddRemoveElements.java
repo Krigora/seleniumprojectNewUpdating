b9dd8a2823881e22f1902720e97f17c6d718f9da
krigora <ihor.kryvynskyy@icloud.com>
2021-11-07 15:03:20 -0600

    updating class TC3_AddRemoveElements

--- src/test/java/com/cybertek/tests/day07_findElements/TC3_AddRemoveElements.java
+++ src/test/java/com/cybertek/tests/day07_findElements/TC3_AddRemoveElements.java
@@ -1,9 +1,24 @@
 package com.cybertek.tests.day07_findElements;
 
 import com.cybertek.utils.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class TC3_AddRemoveElements {
+    /*
+    TC #3: PracticeCybertek.com_AddRemoveElements WebElement verification
+1. Open Chrome browser
+2. Go to http://practice.cybertekschool.com/add_remove_elements
+3. Click to “Add Element” button 50 times
+4. Verify 50 “Delete” button is displayed after clicking.
+5. Click to ALL “Delete” buttons to delete them.
+6. Verify “Delete” button is NOT displayed after clicking.
+USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+Hint: Need to use findElements method.
+     */
 
     public static void main(String[] args) {
         String url = "http://practice.cybertekschool.com/add_remove_elements/";
@@ -11,5 +26,31 @@ public class TC3_AddRemoveElements {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.get(url);
+
+
+//3  locate button 50 times
+        WebElement addElement = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
+
+        //  Click to “Add Element” button 50 times
+        for (int i = 0; i < 50; i++) {
+            addElement.click();
+        }
+
+        // 4. Verify 50 “Delete” button is displayed after clicking.
+        List<WebElement> deleteButtons = driver.findElements(By.xpath("//button[.='Delete']"));
+        System.out.println("count of delete Buttons = " + deleteButtons.size());
+
+        if (deleteButtons.size() == 50){
+            System.out.println("PASS: 50 buttons  added successfully");
+        } else {
+            System.out.println("FAIL: cannot add 50 button");
+        }
+
+        // 5. Click to ALL “Delete” buttons to delete them.
+    //   for (WebElement deleteButton : deleteButtons){ - good working the same deleteButtons.forEach(deleteBtn -> deleteBtn.click());
+    //       deleteButton.click();
+     //  }
+        deleteButtons.forEach(deleteBtn -> deleteBtn.click());  // the same
+
     }
 }
