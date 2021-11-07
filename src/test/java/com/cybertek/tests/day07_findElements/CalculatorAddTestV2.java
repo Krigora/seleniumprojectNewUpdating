b11baac62dfe1afb14b6a9c2584cbd7f4130c1a6
krigora <ihor.kryvynskyy@icloud.com>
2021-11-07 10:57:13 -0600

    creating new class CalculatorAddTestV2

---
+++ src/test/java/com/cybertek/tests/day07_findElements/CalculatorAddTestV2.java
@@ -0,0 +1,39 @@
+package com.cybertek.tests.day07_findElements;
+
+import com.cybertek.utils.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CalculatorAddTestV2 {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        String appUrl = "https://www.calculator.net";
+        int num1 = 7;
+        int num2 = 4;
+        int expectedResult = num1 + num2;
+        driver.get(appUrl);
+
+        System.out.println("//span[.='"+ num1 +"']"); //span[.='7']
+
+        //<span onclick="r(5)" class="scinm">5</span>
+
+        WebElement num1Elem = driver.findElement(By.xpath("//span[.='" + num1 +"']"));
+        WebElement num2Elem = driver.findElement(By.cssSelector("span[onclick='r(" + num2 +")']"));
+        WebElement plus = driver.findElement(By.xpath("//span[.='+']"));
+        WebElement equal = driver.findElement(By.xpath("//span[text()='=']"));
+
+        num1Elem.click();
+        plus.click();
+        num2Elem.click();
+        equal.click();
+
+        WebElement result = driver.findElement(By.xpath("//div[@id='sciOutPut']"));
+        System.out.println("Result = " + result.getText());
+
+        driver.quit();
+
+    }
+}
