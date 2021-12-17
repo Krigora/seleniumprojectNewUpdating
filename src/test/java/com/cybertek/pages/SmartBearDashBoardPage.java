ce75669f6510427e38ba34db7268233e06fc3a75
krigora <ihor.kryvynskyy@icloud.com>
2021-12-16 21:11:40 -0600

    create day20_review_wait and new class

--- src/test/java/com/cybertek/pages/SmartBearDashBoardPage.java
+++ src/test/java/com/cybertek/pages/SmartBearDashBoardPage.java
@@ -1,6 +1,7 @@
 package com.cybertek.pages;
 
 import com.cybertek.utils.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 
 public class SmartBearDashBoardPage {
@@ -8,6 +9,10 @@ public class SmartBearDashBoardPage {
 
     public SmartBearDashBoardPage(){
         PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+    public void getLink(String link){
+        Driver.getDriver().findElement(By.partialLinkText(link)).click();
     }
 
 }
--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -4,6 +4,7 @@ import com.cybertek.utils.Driver;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,6 +13,7 @@ public abstract class TestBase {
     protected WebDriver driver;
     protected Faker faker = new Faker();
     protected Actions actions ;
+    protected WebDriverWait wait = new WebDriverWait(driver,20);
 
     @BeforeMethod
     public void setUp(){
